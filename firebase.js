cc0e3dd9d82ce9050367b21db75f6d9b550fab69
dpatel12 <danyaalp99@gmail.com>
2024-08-16 10:41:16 -0700

    firestore fix

--- firebase.js
+++ firebase.js
@@ -20,6 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFireBase(app);
+const db = getFirestore(app);
 
 export {db}
