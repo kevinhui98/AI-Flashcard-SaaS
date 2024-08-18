39c4c250a78f70340739ead2fad77eafdc95a9cb
kevin <kevinhui98@gmail.com>
2024-08-18 16:18:30 -0400

    adding scroll nav

--- app/components/nav.js
+++ app/components/nav.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { Grid, AppBar, Toolbar, Typography, Button, Stack, Box } from '@mui/material';
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
+import { useRouter, usePathname } from 'next/navigation';
 export const Nav = () => {
-
+    const router = useRouter();
+    const path = usePathname();
     const navLinks = [
         {
             title: 'Home',
@@ -12,33 +14,31 @@ export const Nav = () => {
         },
         {
             title: 'Features',
-            link: '#Features'
+            link: '#features'
         },
         {
             title: 'Pricing',
-            link: '#Pricing'
+            link: '#pricing'
         },
         {
             title: 'Contact',
-            link: '#generate'
+            link: '#contact'
         }
     ]
     return (
         <AppBar position="sticky" sx={{ background: '#0F2A23' }} >
             <Toolbar>
                 <Grid container direction={'row'}>
-                    <Typography variant="h4" style={{ flexGrow: 1 }} color={'#B7EA95'}>
+                    <Typography variant="h4" style={{ flexGrow: 1 }} color={'#B7EA95'} onClick={() => router.push('/')} sx={{ cursor: 'pointer' }}>
                         StockStarter
                     </Typography>
                     <Stack direction="row" spacing={2} style={{ flexGrow: 1 }}>
                         {navLinks.map((navLink, index) => (
-                            <Button color="inherit" key={index} onClick={() => alert(`You are entering ${navLink.title} page`)} >
-                                <Link href={navLink.link} key={index} scroll={true} style={{ "textDecoration": "none", scrollBehavior: 'smooth', color: 'black' }}>
-                                    <Typography variant="h6" letterSpacing={1} color={'#B7EA95'}>
-                                        {navLink.title}
-                                    </Typography>
-                                </Link>
-                            </Button>
+                            <Link href={navLink.link} className={`link ${path === navLinks.link ? 'active' : ''}`} key={index} scroll={true} style={{ "textDecoration": "none", scrollBehavior: 'smooth' }}>
+                                <Typography variant="h6" letterSpacing={1} color={'#B7EA95'}>
+                                    {navLink.title}
+                                </Typography>
+                            </Link>
                         ))}
                     </Stack>
                     <Box mt={0.5}>
